3da8cecddb35b0b859c757a4551769a44843d726
Veinos <Veinoss@hotmail.com>
2024-12-02 08:11:30 -0500

    Fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Bind to all network interfaces, including localhost
+    port: 5173,      // Keep the same port as in your Docker setup
+    strictPort: true // Ensure the port is strictly used or fail if unavailable
+  },
 })
